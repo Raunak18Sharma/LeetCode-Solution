e29a0a39dc498e849bc4c2a878e614a7ee07c311
Raunak Sharma <154464796+Raunak18Sharma@users.noreply.github.com>
2025-06-23 09:53:22 +0530

    Time: 1 ms (93.47%), Space: 42.4 MB (73.58%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
@@ -1,21 +1,21 @@
 class Solution {
+
     public String[] divideString(String s, int k, char fill) {
-        int l = (s.length() / k) + 1;
-        if (s.length() % k == 0)
-            l = s.length() / k;
-        String[] res = new String[l];
-        for (int i = 0; i <= res.length; i++) {
-            if (s.length() >= k) {
-                res[i] = s.substring(0, k);
-                s = s.substring(k);
-            }
+        List<String> res = new ArrayList<>(); // grouped string
+        int n = s.length();
+        int curr = 0; // starting index of each group
+        // split string
+        while (curr < n) {
+            int end = Math.min(curr + k, n);
+            res.add(s.substring(curr, end));
+            curr += k;
         }
-        if (s.length() >= 1) {
-            res[l - 1] = s;
-            for (int i = s.length(); i < k; i++) {
-                res[l - 1] = res[l - 1] + fill;
-            }
+        // try to fill in the last group
+        String last = res.get(res.size() - 1);
+        if (last.length() < k) {
+            last += String.valueOf(fill).repeat(k - last.length());
+            res.set(res.size() - 1, last);
         }
-        return res;
+        return res.toArray(new String[0]);
     }
 }
