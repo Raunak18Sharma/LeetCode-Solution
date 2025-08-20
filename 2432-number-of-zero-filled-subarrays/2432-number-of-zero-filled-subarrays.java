e1fec2a112baa9bf9f83065ba5fa662a835c9999
Raunak Sharma <154464796+Raunak18Sharma@users.noreply.github.com>
2025-08-20 19:52:05 +0530

    Time: 3 ms (94.34%), Space: 61.3 MB (92.31%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -3,14 +3,14 @@ class Solution {
         long count = 0;          // total subarrays
         long currentZeros = 0;   // length of current zero streak
 
-        for (int num : nums) {
-            if (num == 0) {
-                currentZeros++;
-                count += currentZeros;  // each new zero extends subarrays
-            } else {
-                currentZeros = 0;       // reset when non-zero found
-            }
-        }
+       for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+        currentZeros++;
+        count += currentZeros;
+    } else {
+        currentZeros = 0;
+    }
+}
 
         return count;
     }
